47d58c9884c484ad8b1e8f9363c760f836cc233a
Travis <travis@devmaterial.com>
2018-10-28 07:40:11 -0400

    [add] functionality

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-const guide = ['a', 'e', 'i', 'o', 'u'];
-
-let userPosition = 0;
+const guide = ("Summer evenings spent looking at the magical night sky remind us of the beauty of the universe. The vastness of space seems to go on forever and remain unchanging, perfect.").split('');
 
 let div = document.querySelector('div');
 
-// For each letter
-guide.forEach((letter, i) => {
-	// Create a span element
-  let el = document.createElement('span');
-  
-  // give element a class name
-  el.className = i;
-  
-  // Append letter to span element
-  el.innerHTML += letter;
+class Sprint {
+  constructor(sprint) {
+    this.sprint = sprint;
+    this.position = 0;
+    this.currentCharacter;
+    this.init();
+  }
+  init() {
+    this.generateLetters();
+    this.placeTypeFace(this.position);
+    document.addEventListener('keypress', e => this.keyListener(e));
+  }
+  keyListener(e) {
+    const code = e.keyCode || e.which,
+      str = String.fromCharCode(code),
+      currentChar = this.sprint[this.position],
+      el = document.getElementsByClassName(this.position)[0];
 
-  // Stick span in div
-  div.appendChild(el);
-});
-	
-
-let keyListener = (e) => {
-
-	const code = e.keyCode || e.which,
-  			str = String.fromCharCode(code),
-        currentChar = guide[userPosition],
-        el = document.getElementsByClassName(userPosition)[0];
-  
-  console.log(el);
-  if (str === currentChar) {
-    el.style.color = '#e5e5e5'
-    userPosition++
-  } else {
-  	el.style.color = 'red'
+    if (str === currentChar) {
+      this.removeTypeFace(el);
+      this.placeTypeFace(this.position + 1);
+      this.position++
+    } else {
+      this.incorrectLetter(el);
+    }
+  }
+  placeTypeFace(pos) {
+    const span = document.getElementsByClassName(pos)[0];
+    span.style.background = 'orange';
+  }
+  removeTypeFace(el) {
+    el.style.color = '#e5e5e5';
+    el.style.background = 'white';
+  }
+  incorrectLetter(el) {
+    el.style.color = 'red';
+  }
+  generateLetters() {
+    this.sprint.forEach((letter, i) => {
+      let el = document.createElement('span');
+      el.className = i;
+      el.innerHTML += letter;
+      div.appendChild(el);
+    });
   }
 }
 
-document.addEventListener('keypress', keyListener);
+new Sprint(guide);
